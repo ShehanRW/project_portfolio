1b02e748346d9218bfdd91e7dd8f48ba84e2c7fd
Shehan wijeratne <shehanranjukaw5@gmail.com>
2024-06-28 02:16:55 +0530

    dashboard components design

---
+++ app/components/dash_components/InputBlog.tsx
@@ -0,0 +1,22 @@
+import { Button, Flex, Input, Textarea, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const InputBlog = () => {
+  return (
+    <>
+        <Flex w={900} direction={'column'} m={10} border={'solid 2px'} borderColor={'brand.200'} padding={2} gap={1}>
+            <Text color={'brand.200'}>Add a blog</Text>
+            <Button bg={'brand.200'} size={'sm'} borderRadius={'none'} w={200}>Add image</Button>
+            <Input placeholder='Blog Title' size={'sm'} color={'brand.200'} borderRadius={'none'}/>
+            <Textarea placeholder='Content' size={'sm'} color={'brand.200'} borderRadius={'none'}/>
+            <Flex gap={1}>
+                <Button bg={'brand.200'} size={'sm'} borderRadius={'none'} w={200}>Save</Button>
+                <Button bg={'brand.200'} size={'sm'} borderRadius={'none'} w={200}>Edit</Button>
+                <Button bg={'brand.200'} size={'sm'} borderRadius={'none'} w={200}>Delete</Button>
+            </Flex>
+        </Flex>
+    </>
+  )
+}
+
+export default InputBlog
--- app/dash/sheedragx/page.tsx
+++ app/dash/sheedragx/page.tsx
@@ -1,8 +1,23 @@
+import HomeStatus from '@/app/components/dash_components/HomeStatus'
+import InputBlog from '@/app/components/dash_components/InputBlog'
+import InputProjects from '@/app/components/dash_components/InputProjects'
+import { Button, Flex, Input, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const Dash = () => {
   return (
-    <div>page</div>
+    <>
+      <Flex>
+        <HomeStatus/>
+        
+      </Flex>
+      <Flex>
+        <InputProjects/>
+      </Flex>
+      <Flex>
+        <InputBlog/>
+      </Flex>
+    </>
   )
 }
 
