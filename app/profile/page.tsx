963fc152d741dfdafa1ffa6da60ebe4e25039fa6
Shehan wijeratne <shehanranjukaw5@gmail.com>
2024-06-27 19:48:09 +0530

    profile content design

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,17 +7,17 @@ const ProfilePage = () => {
     <>
       <HeaderAppBar/>
       <Flex h={"60px"}></Flex>
-      <Flex direction={{base:'column', md:'row'}} justifyContent={'center'} >
-        <Box mx={5}>
-          <Image src='profile_pic.jpg' alt='profile_pic' w={500}/>
+      <Flex direction={{base:'column', md:'row'}} >
+        <Box mx={5} >
+          <Image src='profile_pic.jpg' alt='profile_pic' w={{base:'auto', md:800}}/>
         </Box>
 
-        <Flex direction={'column'} m={10} >
+        <Flex direction={'column'} m={10}>
           <Text color={'brand.200'} fontSize={{base:40,md:50}} fontWeight={10}>Shehan Wijerathna</Text>
           <Text color={'brand.200'} fontSize={15} fontWeight={500} ml={5}>Software Developer Reading Bachelors </Text>
-          <Text color={'brand.200'} fontSize={20} fontWeight={10} m={5} w={500}>I am interested in software developing, system architectures and coding. I am doing arts, 3D design and video production to youtube as a hobby also. </Text>
+          <Text color={'brand.200'} fontSize={20} fontWeight={10} m={5}>I am interested in software developing, system architectures and coding. I am doing arts, 3D design and video production to youtube as a hobby also. </Text>
 
-          <Flex m={5} direction={'column'} >
+          <Flex m={5} direction={'column'}>
             <Text color={'brand.200'} fontSize={15} fontWeight={10}>Mobile: +94762782251</Text>
             <Link href='mailto:shehanranjukaw5@gmail.com' color={'brand.200'} fontSize={15} fontWeight={10}>Email</Link>
             <Link href='https://www.linkedin.com/in/shehan-wijerathne-66124120a/' target='blank' color={'brand.200'} fontSize={15} fontWeight={10}>LinkedIn</Link>
