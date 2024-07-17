5d8715a5e65834816615e653514b1a7f5eea3582
Shehan wijeratne <shehanranjukaw5@gmail.com>
2024-07-17 23:46:53 +0530

    add favicon

--- app/_app.tsx
+++ app/_app.tsx
@@ -2,6 +2,7 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
+import { Head } from 'next/document';
 
 function MyApp({ Component, pageProps }: AppProps) {
     const router = useRouter();
@@ -11,7 +12,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         router.push('/home');
     }, [router]);
 
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+            </Head>
+        <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
